5cd0270736f69c37a751065f882f2dbfbc7c4780
Chandanjha21 <ck8448470734@gmail.com>
2024-06-24 17:17:20 +0530

    COmitdfgd

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const express = require('express')
-
 const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost:27017/' , {
+    dbName: "Saleperson"
+})
+.then(
+    () => {console.log("database connected")}
+).catch(
+    (e) => {console.log(e)}
+)
+
+const userSchema = new mongoose.Schema({
+    name : String , 
+    email : String,
+    role : String ,
+})
+
+const userModel = mongoose.model('Users' , userSchema)
+
+app.get('/add' , (req , res) => {
+    userModel.create({name: "Chandan" , email: "ckj@gmail.com" , role : "admin"}).then(() => {
+        console.log("data succesfully added to userModel");
+        res.send("Congrats its added");
+    }).catch((e) => {
+        console.log(e);
+    })
+})
 
 app.listen(5000 , () => {
-    console.log("Listening at port")
+    console.log("Listening at port 5000")
 })
