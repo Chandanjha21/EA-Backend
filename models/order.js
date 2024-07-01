bedd2b7fa3d4f7c7934032fac0dcfdf4b21b6e7b
Chandanjha21 <chandankumarjha6069@gmail.com>
2024-07-01 22:50:58 +0530

    models/order.js

--- models/order.js
+++ models/order.js
@@ -1,7 +1,33 @@
 import mongoose from "mongoose";
 
+const itemSchema = new mongoose.Schema({
+    name : String ,
+    size : String ,
+    quantity : Number ,
+    Price : Number
+})
+
 //here we will write all the order schema and all
 const orderSchema = new mongoose.Schema({
+
+    // IF not sure about the type you can also use mongoose.Schema.Types.Mixed
+
+    orderId : { //Considering order Id and Bill number will be same
+        type : Number, 
+        required : true,
+    },
+    salesman : {
+        type : String,
+        required : true,
+    },
+    buyer : {
+        type : String , 
+        required : true,
+    },
+    items : {
+        type : [itemSchema], //You can actually use this type as well
+        required : true,
+    },
     createdAt : {
         type : Date , 
         default : now ,
